9355db1313250e4e02983d5a1fc2f0b1d8a9efa4
dailt <dai.luong.the@sobanhang.com>
2022-04-25 01:09:44 +0700

    up

--- docs/docs.go
+++ docs/docs.go
@@ -738,7 +738,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:8080",
+	Host:             "api.uchain.duckdns.org",
 	BasePath:         "/v1",
 	Schemes:          []string{},
 	Title:            "Finan Loyalty API",
